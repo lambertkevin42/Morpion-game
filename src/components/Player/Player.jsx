079672b994c954671315a253c7a86e22aa8d31a6
lambertkevin42 <lambertkevin1994@gmail.com>
2024-01-25 13:31:27 +0100

    added a 'editer' button with his css

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -4,12 +4,18 @@ function Player() {
     return(
         <ol id="players">
             <li>
-                <span className="player-name">Player 1</span>
-                <span className="player-symbol">X</span>
+                <span className="player">
+                    <span className="player-name">Player 1</span>
+                    <span className="player-symbol">X</span>
+                </span>
+                <button>Editer</button>
             </li>
             <li>
-                <span className="player-name">Player 2</span>
-                <span className="player-symbol">O</span>
+            <span className="player">
+                    <span className="player-name">Player 2</span>
+                    <span className="player-symbol">O</span>
+                </span>
+                <button>Editer</button>
             </li>
         </ol>
     );
