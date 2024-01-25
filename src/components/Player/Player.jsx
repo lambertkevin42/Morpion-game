d2c4377604a6ed94c04792a9343c857de16817cb
lambertkevin42 <lambertkevin1994@gmail.com>
2024-01-25 14:32:38 +0100

    handleChange and conditionnal content

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -3,27 +3,31 @@
 import { useState } from "react";
 import "./player.css";
 
-export default function Player({ name, symbol }) {
-
+export default function Player({ initialName, symbol }) {
+    const [ playerName, setPlayerName ] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(true);
+        setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if (isEditing ) {
-        playerName = <input type="text" requiered />       
+        editablePlayerName = <input type="text" requiered value={playerName} onChange={handleChange} />       
     }
 
     return(
         <li className="player">
             <span className="player">
-              {playerName}
+              {editablePlayerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Editer</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Sauvegarder' : 'Editer'}</button>
         </li>
     );
 }
